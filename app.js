432c949a4bc8deb38267209be749490f17be707c
Oliver Weber <oliver@mondough.io>
2020-03-11 14:36:46 +0100

    apply logger middleware to /api routes

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 
+const logger = require("./middleware/logger");
+
 const app = express();
 
 // Routes
 const studentRoutes = require("./routes/students");
 
 app.use(express.json());
+app.use("/api", logger);
 app.use("/api/students", studentRoutes);
 
 module.exports = app;
